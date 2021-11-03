9fc6128a48a996afe9d334574e052535dca822ee
栗田大輔 <daikons.msn36@gmail.com>
2021-11-03 10:30:24 +0900

    ある年にもっとも勝利したピッチャーのplayer_idを配列で取得するメソッドを記述

--- app/models/pitcher.rb
+++ app/models/pitcher.rb
@@ -1,2 +1,6 @@
 class Pitcher < ApplicationRecord
+  def self.most_winners(year)
+    maximum_wins =  where(year_id: year).maximum(:wins)
+    where(year_id: year, wins: maximum_wins).pluck(:player_id)
+  end
 end
